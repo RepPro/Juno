c0f7c52a66663fdb6439cd528e6cb5266bc9330e
Christopher Ostrowski <chris@madebyfunction.com>
2017-02-21 08:58:25 -0600

    Added on error hook to transact. Bumped version.

--- lib/Juno/active_record_builder.rb
+++ lib/Juno/active_record_builder.rb
@@ -2,10 +2,17 @@ require 'active_record'
 
 module Juno
   module ActiveRecordBuilder
+    extend ActiveSupport::Concern
+
+    included do
+      attr_reader :errors
+    end
+
     def transact!
       ActiveRecord::Base.transaction {yield}
     rescue => e
       errors.push e.message
+      on_call_error(e) if respond_to?(:on_call_error)
       return false
     end
   end
